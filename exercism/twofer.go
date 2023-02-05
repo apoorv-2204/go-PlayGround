45aac9a8c37a6ed75ca865b7a08d33067487ffb6
apoorv-2204 <apoorva@uniris.io>
2023-02-05 23:43:05 +0530

    initial excercism

---
+++ exercism/twofer.go
@@ -0,0 +1,13 @@
+package exercism
+
+import (
+	"fmt"
+)
+
+func ShareWith(name string) string {
+	if name == "" {
+		return "One for you, one for me."
+	} else {
+		return fmt.Sprintf("One for %s, one for me.", name)
+	}
+}
