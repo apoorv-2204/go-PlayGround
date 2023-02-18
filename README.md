5364a1cfa0c3aebb08e2cc75b218daaacccf554b
apoorv-2204 <apoorva@uniris.io>
2023-02-18 10:58:22 +0530

    temp

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 Just another go playground
 
--https://github.com/golang-standards/project-layout
- - https://tutorialedge.net/golang/go-project-structure-best-practices/
+- https://github.com/golang-standards/project-layout
+- https://tutorialedge.net/golang/o-project-structure-best-practices/
+- https://golangbot.com/go-packages/
+- import alias "import/path"  alias.Method() https://go.dev/doc/code
+
+
+
+
+
+
+- https://unix.stackexchange.com/questions/322307/running-command-multiple-times-and-trigger-report-on-failure
+- https://unix.stackexchange.com/questions/22726/how-to-conditionally-do-something-if-a-command-succeeded-or-failed
+- https://go.dev/doc/code
--- main.go
+++ main.go
@@ -1,24 +1,94 @@
 package main
 
 import (
-	"go_playground/dsa"
+	"fmt"
+	"log"
 )
-// import alias "import/path"
-// alias.Method()
 
 func main() {
+	arrBS := []int{
+		1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10,
+	}
+	arr2 = []int{0, 1, 2, 3, 4, 5, 6, 8, 9, 10}
 
-	// var str1 string
-	// var size int
-	// _, err := fmt.Scanln(&size, &str1)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	fmt.Println(BinarySearch(arrBS, 11))
+	fmt.Println(LowerBound(arrBS, 3))
+	fmt.Println(UpperBound(arrBS, 4))
 
-	dsa.PrintDiamond(5, "x")
-	dsa.PrintFullPryamid(5, "x")
-	dsa.PrintNotOfHollowPryamid(5, "+")
+}
 
+// with binary search
+func FirstMissingElement(arr []int, target int) int {
+
+}
+
+// first occurence of target element
+func LowerBound(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+	var index int = -1
+	for start <= end {
+		var mid int = start + (end-start)/2
+		if arr[mid] == target {
+			index = mid
+			end = mid - 1
+		} else if target < arr[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+
+	return index
+
+}
+
+// last occurence in sorted arrry
+func UpperBound(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+	var index int = -1
+	for start <= end {
+		var mid int = start + (end-start)/2
+		if arr[mid] == target {
+			index = mid
+			start = mid + 1
+		} else if target < arr[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+
+	return index
+
+}
+
+func BinarySearch(arr []int, target int) int {
+	start := 0
+	end := len(arr) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if arr[mid] == target {
+			return mid
+		} else if target < arr[mid] {
+			end = mid - 1
+		} else if target > arr[mid] {
+			start = mid + 1
+		}
+	}
+	return -1
+}
+
+func TakeInput() {
+	var str1 string
+	var size int
+	_, err := fmt.Scanln(&size, &str1)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // binary as it splits searcg space in two
